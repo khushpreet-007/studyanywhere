8d862d48341435d0a072168be0544a00569fab57
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2022-06-18 09:53:33 +0530

    Update settings.py

--- codewithkhushpreet/settings.py
+++ codewithkhushpreet/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / '/static'
 
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
